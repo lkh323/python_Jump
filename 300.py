b582b24fda71ca0ca1f6bbe6aad3a5e816ab46c8
lkh323 <lkh323@gmail.com>
2025-12-30 13:36:45 +0900

    300 문제 파일 추가

---
+++ 300.py
@@ -0,0 +1,53 @@
+#001 시작하기
+
+print("Hello, World!")
+
+print("Mary's cosmetics")
+
+print(""" 신씨가 소리질렀다. "도둑이야". """)
+
+print("C:\\Windows")
+
+print("안녕하세요.\n만나서\t\t반갑습니다.")
+
+print ("오늘은", "일요일")
+
+print("naver", "kakao", "sk", sep=";")
+
+print("naver", "kakao", "sk", sep="/")
+
+print("first"); print("second")
+print("first", end=" "); print("second")
+
+print(5/3)
+
+
+
+#002 변수수
+
+삼성전자=50000
+총평가금액=삼성전자*10
+print(총평가금액)
+
+시가총액, 현재가, PER = 298000000000000, 50000, 15.79
+print(시가총액, 현재가, PER)
+
+s = "hello"
+t = "python"
+print(s + "!", t)
+
+
+print(2 + 2*3)
+
+
+a = "132"
+print(type(a))
+
+
+num_str = "720"
+int_num = int(num_str)
+print(int_num, type(int_num))
+
+
+#003 문자열
+
